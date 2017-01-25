dd6969f378522a9b98153fb7b084d188e713ce6a
rahulworld <chauhanr371@gmail.com>
2017-01-25 22:47:33 +0530

    added

---
+++ newCompiler.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+string b;
+int main(){
+    freopen("test.txt","r",stdin);
+    while(1){
+    //cin>>a;
+    if(!getline(cin,b))
+        break;
+    else
+    cout<<b.find("//")<<endl;
+    //cout<<regex_replace(b,regex("->"), ".")<<endl;
+    //str.replace(str.begin()+11,str.end(),str4.begin(),str4.end());
+    //replace( b.begin(),b.find("->"), "->", "->");
+    }
+    return 0;
+}
+
+
+//freopen("test.txt","r",stdin);
+    //while(1){
+    //cin>>a;
+//    if(!getline(cin,b))
+    //        break;
+    //else
+    //cout<<b.find("//")<<endl;
+    //cout<<regex_replace(b,regex("->"), ".")<<endl;
+    //str.replace(str.begin()+11,str.end(),str4.begin(),str4.end());
+    //replace( b.begin(),b.find("->"), "->", "->");
+    //}
